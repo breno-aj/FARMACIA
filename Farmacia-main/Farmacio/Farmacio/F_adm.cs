e7ebde70f0dec1ec030619a52e50b4b2c65c697b
fernandaduarten <fernanda440195@gmail.com>
2025-06-09 15:39:05 -0300

    aula5

--- Farmacia-main/Farmacio/Farmacio/F_adm.cs
+++ Farmacia-main/Farmacio/Farmacio/F_adm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -30,7 +31,36 @@ namespace Farmacio
 
         private void btncadrastar_Click(object sender, EventArgs e)
         {
+            // Conectar ao banco de dados
+            // codigo de cadastro de funcionarios
+            String strconn = @"Data Source=;Initial Catalog=Senac;Integrated Security = True";
+                SqlConnection conn = new SqlConnection(strconn);
+            string sql = "INSERT INTO funcionarios (nome, cpf, data de nascimento, e-mail, cargos, usuario, senha) " +
+                "VALEU (@nome, @cpf, @datanascimento, @email, @cargos, @usuario, @senha)";
+            try
+            {
+                SqlCommand comando = new SqlCommand(sql, conn);
+                comando.Parameters.Add(new SqlParameter("@nome", tbxnome.Text));
+                comando.Parameters.Add(new SqlParameter("@cpf", tbxcpf.Text));
+                comando.Parameters.Add(new SqlParameter("@datanascimento", tbxdata.Text));
+                comando.Parameters.Add(new SqlParameter("@email", tbxemail.Text));
+                comando.Parameters.Add(new SqlParameter("@cargos", cbxcargos.Text));
+                comando.Parameters.Add(new SqlParameter("@usuario", tbxusuario.Text));
+                comando.Parameters.Add(new SqlParameter("@senha", tbxsenha.Text));
 
+                conn.Open();
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Funcionario Cadastrado com sucesso!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possivel  cadastrar", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Closed();
+            }
         }
     }
 }
